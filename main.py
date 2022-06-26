3f2b5f535d475832fe070a7b0b9ceabdea3776b4
Shubham Patil <shubhampatilsd@gmail.com>
2022-06-26 15:55:04 -0700

    added some styling

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ import tkinter as tk
 
 # LE_IMAGE = None
 
-w1=tk.Tk()
+w1 = tk.Tk()
 w1.title("Sketchy")
 w1.geometry("480x320")
-label =tk.Label(w1)
-label.grid(row=0, column=0)
+label = tk.Label(w1)
+label.grid(row=0, column=3)
 cap = cv2.VideoCapture(0)
 
 
 def getImage():
     # while(True):
-        # time.sleep(1)
-    ret,frame = cap.read()
+    # time.sleep(1)
+    ret, frame = cap.read()
     # cv2.imshow('frame', frame)
-    
+
     image = frame
     grey_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(grey_img, (21, 21), 0)
@@ -46,13 +46,14 @@ def getImage():
     background.save("main.png")
 
     return ImageTk.PhotoImage((background))
-        # break
+    # break
 
     # cap.release()
     # cv2.destroyAllWindows()
 
+
 def updateLabel():
-    LE_IMAGE=getImage()
+    LE_IMAGE = getImage()
     label.imgtk = LE_IMAGE
     label.configure(image=LE_IMAGE)
    # Repeat after an interval to capture continiously
@@ -62,33 +63,37 @@ def updateLabel():
 def postTweet():
     media = api.media_upload('./main.png')
     api.update_status("CodeDay", media_ids=[media.media_id_string])
+
+
 def showWindow():
-   
-#    while True:
-#    while True:
-   
-#    print(LE_IMAGE)
 
-   updateLabel();
-   for i in range(3):
-       w1.columnconfigure(i, weight=1)
-   w1.rowconfigure(1, weight=1)
+    #    while True:
+    #    while True:
 
-# bird = tk.Button(w1, text="Post to Twitter")
-# bird.grid(row=2, column=1)
+    #    print(LE_IMAGE)
 
-# retake = tk.Button(w1, text="Retake")
-# retake.grid(row=2, column=2)
+    for i in range(6):
+        w1.columnconfigure(i, weight=1)
+    w1.rowconfigure(1, weight=1)
 
-# printer = tk.Button(w1, text="Print photo")
-# printer.grid(row=2, column=3)
+    bird = tk.Button(w1, text="Post to Twitter", command=postTweet)
+    bird.grid(row=2, column=3, sticky='nesw')
+
+    # retake = tk.Button(w1, text="Retake")
+    # retake.grid(row=2, column=1, sticky='nesw')
+
+    printer = tk.Button(w1, text="Print photo")
+    printer.grid(row=2, column=4, sticky='nesw')
+
+    updateLabel()
+
 
 #    label=tk.Label(w1,image=LE_IMAGE).pack()
 
-#    updateLabel(label, LE_IMAGE)
-   w1.mainloop()
 
-   
+#    updateLabel(label, LE_IMAGE)
+    w1.mainloop()
+
 
 if __name__ == "__main__":
     showWindow()
