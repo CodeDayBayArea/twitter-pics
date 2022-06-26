4e013c8cb7171353fa877fdc1b7babecdb64459e
Shubham <shubhampatilsd@gmail.com>
2022-06-26 15:12:00 -0700

    HOLY MOLY IT WORKS

--- main.py
+++ main.py
@@ -17,38 +17,46 @@ import tkinter as tk
 
 # LE_IMAGE = None
 
+w1=tk.Tk()
+w1.title("Sketchy")
+w1.geometry("480x320")
+label =tk.Label(w1)
+label.grid(row=0, column=0)
+cap = cv2.VideoCapture(0)
+
 
 def getImage():
-    cap = cv2.VideoCapture(0)
-    while(True):
+    # while(True):
         # time.sleep(1)
-        ret,frame = cap.read()
-        # cv2.imshow('frame', frame)
-        
-        image = frame
-        grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(grey_img, (21, 21), 0)
-        sketch = cv2.divide(grey_img, blur, scale=256.0)
-        cv2.imwrite("sketch.png", sketch)
+    ret,frame = cap.read()
+    # cv2.imshow('frame', frame)
+    
+    image = frame
+    grey_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(grey_img, (21, 21), 0)
+    sketch = cv2.divide(grey_img, blur, scale=256.0)
+    cv2.imwrite("sketch.png", sketch)
 
-        background = Image.open("sketch.png").convert("RGBA")
-        width, height = background.size
-        foreground = Image.open("stamp.png").convert("RGBA")
-        foreground = foreground.resize(
-            (width, int(150*(height/width))))
-        background.paste(foreground, (0, 0), mask=foreground)
-        background.save("main.png")
+    background = Image.open("sketch.png").convert("RGBA")
+    width, height = background.size
+    foreground = Image.open("stamp.png").convert("RGBA")
+    foreground = foreground.resize(
+        (width, int(150*(height/width))))
+    background.paste(foreground, (0, 0), mask=foreground)
+    background.save("main.png")
 
-        return ImageTk.PhotoImage((background))
-        break
+    return ImageTk.PhotoImage((background))
+        # break
 
     # cap.release()
     # cv2.destroyAllWindows()
 
-def updateLabel(label,LE_IMAGE):
+def updateLabel():
+    LE_IMAGE=getImage()
+    print("hi")
     label.imgtk = LE_IMAGE
     label.configure(image=LE_IMAGE)
-    # Repeat after an interval to capture continiously
+   # Repeat after an interval to capture continiously
     label.after(20, updateLabel)
 
 
@@ -56,13 +64,13 @@ def postTweet():
     media = api.media_upload('./main.png')
     api.update_status("CodeDay", media_ids=[media.media_id_string])
 def showWindow():
-   w1=tk.Tk()
-   w1.title("Sketchy")
-   w1.geometry("480x320")
+   
 #    while True:
 #    while True:
-   LE_IMAGE=getImage()
-   print(LE_IMAGE)
+   
+#    print(LE_IMAGE)
+
+   updateLabel();
    for i in range(3):
        w1.columnconfigure(i, weight=1)
    w1.rowconfigure(1, weight=1)
@@ -76,9 +84,9 @@ def showWindow():
 # printer = tk.Button(w1, text="Print photo")
 # printer.grid(row=2, column=3)
 
-   label=tk.Label(w1,image=LE_IMAGE).pack()
+#    label=tk.Label(w1,image=LE_IMAGE).pack()
 
-   updateLabel(label, LE_IMAGE)
+#    updateLabel(label, LE_IMAGE)
    w1.mainloop()
 
    
