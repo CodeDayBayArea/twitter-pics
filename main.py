e8c3a24b5fcdaa3dd6400105c767259bf496734d
Shubham <shubhampatilsd@gmail.com>
2022-06-26 15:03:16 -0700

    something ill work on later

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tweepy
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import cv2
+import time
 from PIL import Image, ImageTk
-import tkinter as tki
-from __future__ import print_function
+import tkinter as tk
+# from __future__ import print_function
 # load_dotenv()
 
 # auth = tweepy.OAuthHandler(os.environ.get(
@@ -14,12 +15,16 @@ from __future__ import print_function
 
 # api = tweepy.API(auth)
 
+# LE_IMAGE = None
+
 
 def getImage():
     cap = cv2.VideoCapture(0)
     while(True):
-        frame = cap.read()
-        cv2.imshow('frame', frame)
+        # time.sleep(1)
+        ret,frame = cap.read()
+        # cv2.imshow('frame', frame)
+        
         image = frame
         grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(grey_img, (21, 21), 0)
@@ -33,10 +38,20 @@ def getImage():
             (width, int(150*(height/width))))
         background.paste(foreground, (0, 0), mask=foreground)
         background.save("main.png")
+
+        return ImageTk.PhotoImage((background))
         break
 
-    cap.release()
-    cv2.destroyAllWindows()
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+def updateLabel(label,LE_IMAGE):
+    label.imgtk = LE_IMAGE
+    label.configure(image=LE_IMAGE)
+    # Repeat after an interval to capture continiously
+    label.after(20, updateLabel)
+
+
 def postTweet():
     media = api.media_upload('./main.png')
     api.update_status("CodeDay", media_ids=[media.media_id_string])
@@ -44,20 +59,29 @@ def showWindow():
    w1=tk.Tk()
    w1.title("Sketchy")
    w1.geometry("480x320")
+#    while True:
+#    while True:
+   LE_IMAGE=getImage()
+   print(LE_IMAGE)
    for i in range(3):
-     w1.columnconfigure(i, weight=1)
+       w1.columnconfigure(i, weight=1)
    w1.rowconfigure(1, weight=1)
 
-   bird = tk.Button(w1, text="Post to Twitter")
-   bird.grid(row=2, column=1)
+# bird = tk.Button(w1, text="Post to Twitter")
+# bird.grid(row=2, column=1)
 
-   retake = tk.Button(w1, text="Retake")
-   retake.grid(row=2, column=2)
+# retake = tk.Button(w1, text="Retake")
+# retake.grid(row=2, column=2)
 
-   printer = tk.Button(w1, text="Print photo")
-   printer.grid(row=2, column=3)
+# printer = tk.Button(w1, text="Print photo")
+# printer.grid(row=2, column=3)
 
+   label=tk.Label(w1,image=LE_IMAGE).pack()
+
+   updateLabel(label, LE_IMAGE)
    w1.mainloop()
+
+   
 
 if __name__ == "__main__":
     showWindow()
